237d6ebffd93122bd0eab4bb567a8f0482040076
Lithi <lithim11@gmail.com>
2022-11-04 09:16:39 +0200

    Responsiveness for landscape phone devices and resizing certificates

--- src/components/Abilities/Certifications.jsx
+++ src/components/Abilities/Certifications.jsx
@@ -3,14 +3,13 @@ import sqlCertificate from "../../media/Certificates/SQL_Certificate.pdf";
 import googleAdsCertificate from "../../media/Certificates/Google_Ads_Certification.pdf";
 
 function Certifications() {
-  const height = 350;
   return (
     <div id="ContentContainer">
       <div className="Certificate">
         <h4>SQL Certificate</h4>
         <a href="https://www.sololearn.com/certificates/CT-UJ828G0U" target="_blank" rel="noopener noreferrer">
           <Document classname="doc" file={sqlCertificate}>
-              <Page height={height} pageNumber={1} />
+              <Page pageNumber={1} />
           </Document>
         </a>
       </div>
@@ -18,7 +17,7 @@ function Certifications() {
       <div className="Certificate">
         <h4>Google Ads Display Certificate</h4>
         <Document classname="doc" file={googleAdsCertificate}>
-            <Page height={height} pageNumber={1} />
+            <Page pageNumber={1} />
         </Document>
       </div>
     </div>
